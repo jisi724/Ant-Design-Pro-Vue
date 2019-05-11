50412780f2e6bb88097204d2b037e407f55f2ca7
EthanZ <ezhang@phoceis.asia>
2019-05-11 14:39:27 -0700

    automatic form validation

--- src/views/Forms/BasicForm.vue
+++ src/views/Forms/BasicForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a-form :layout="formLayout">
+    <a-form :layout="formLayout" :form="form">
       <a-form-item
         label="Form Layout"
         :label-col="formItemLayout.labelCol"
@@ -25,19 +25,33 @@
         label="Field A"
         :label-col="formItemLayout.labelCol"
         :wrapper-col="formItemLayout.wrapperCol"
-        :validateStatus="fieldAStatus"
-        :help="fieldAHelp"
       >
-        <a-input v-model="fieldA" placeholder="input placeholder" />
+        <a-input
+          v-decorator="[
+            'fieldA',
+            {
+              initialValue: fieldA,
+              rules: [{ required: true, min: 6, message: 'Must over 6 chars' }]
+            }
+          ]"
+          placeholder="input placeholder"
+        />
       </a-form-item>
       <a-form-item
         label="Field B"
         :label-col="formItemLayout.labelCol"
         :wrapper-col="formItemLayout.wrapperCol"
-        :validateStatus="fieldBStatus"
-        :help="fieldBHelp"
       >
-        <a-input v-model="fieldB" placeholder="input placeholder" />
+        <a-input
+          v-decorator="[
+            'fieldB',
+            {
+              initialValue: fieldB,
+              rules: [{ required: true, min: 6, message: 'Must over 6 chars' }]
+            }
+          ]"
+          placeholder="input placeholder"
+        />
       </a-form-item>
       <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
         <a-button type="primary" @click="handleSubmit">
@@ -51,26 +65,12 @@
 <script>
 export default {
   data() {
+    this.form = this.$form.createForm(this);
     return {
       formLayout: "horizontal",
       fieldA: "",
-      fieldAStatus: "",
-      fieldAHelp: "",
-      fieldB: "",
-      fieldBStatus: "",
-      fieldBHelp: ""
+      fieldB: ""
     };
-  },
-  watch: {
-    fieldA(val) {
-      if (val.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "Must over 5 chars";
-      } else {
-        this.fieldAStatus = "";
-        this.fieldAHelp = "";
-      }
-    }
   },
   computed: {
     formItemLayout() {
@@ -96,15 +96,16 @@ export default {
       this.formLayout = e.target.value;
     },
     handleSubmit() {
-      if (this.fieldA.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "Must over 5 chars";
-      } else {
-        console.log({
-          fieldA: this.fieldA,
-          fieldB: this.fieldB
-        });
-      }
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values);
+          // direct assign value
+          // this.fieldA = values.fieldA
+
+          // or batch assign value
+          Object.assign(this, values);
+        }
+      });
     }
   }
 };
